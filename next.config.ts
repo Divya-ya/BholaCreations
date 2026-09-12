f6912e9250645b4aea80b9a4f33e230cc0904b61
unknown <divnaudi2003@gmail.com>
2026-09-12 22:20:48 +0530

    Configure next-on-pages adapter

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
   allowedDevOrigins: ["192.168.29.19"],
 };
 
